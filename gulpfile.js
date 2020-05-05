06d507d18310ffa038ad52110a4eabf24d606c2d
Erik Schirato <eriks@ciandt.com>
2020-05-05 12:54:22 -0300

    [iBringWeb] Ajustes no gulp e sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
 gulp.task('sass', gulp.series(function () {
   return gulp.src(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'])
     .pipe(sass()) // converter o Sass em CSS
-    .pipe(gulp.dest('src/css'));
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
 }));
 
 gulp.task('watch', gulp.series(function () {
@@ -18,6 +20,7 @@ gulp.task('browser-sync', function () {
       baseDir: "./src"
     }
   });
+  gulp.watch(["src/*.html", "src/scss/*.scss"]).on("change", reload);
 });
 
-gulp.task('default', gulp.series(['sass', 'browser-sync']));
+gulp.task('default', gulp.series(['sass', 'watch']));
